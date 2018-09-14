be8a145dc7920f38d8ee32080bd0b30b37d27bdb
Mayank Madhok <mayank.madhok@uk.ey.com>
2018-09-14 16:21:33 +0100

    footer work

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 
-//var sourseUrl = "src.json";
+var sourseUrl = "src.json";
 
-var sourseUrl = "https://sites.ey.com/sites/digitalassets/_api/web/Lists/getByTitle('database')/items?$top=2000";
+// var sourseUrl = "https://sites.ey.com/sites/digitalassets/_api/web/Lists/getByTitle('database')/items?$top=2000";
 //
 //
 //  var sourseUrl = "https://sites.ey.com/sites/digitalassets/_api/web/Lists/getByTitle('database')/items?$top=2000&$expand=AttachmentFiles";
